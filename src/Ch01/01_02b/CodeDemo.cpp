aa2172f777ab74886f12a7f611471de3ba4d70f2
Corbin55 <corbinhawke5@gmail.com>
2025-09-10 02:08:02 +0000

    finished after video

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,14 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream> //part of c++ library for IO
+/*
+    just like java, multi line comments are this
+*/
+int main(){
+    std::cout << "Hi there!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
